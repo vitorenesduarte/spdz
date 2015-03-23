f960fd78f3edeed287724c6ab678b2152cbb0732
Vitor Enes Duarte <vitorenesduarte@gmail.com>
2015-03-23 20:21:14 +0000

    Update CircuitGenerator

--- spdz/src/main/java/sdc/spdz/circuits/CircuitGenerator.java
+++ spdz/src/main/java/sdc/spdz/circuits/CircuitGenerator.java
@@ -1,7 +1,6 @@
 package sdc.spdz.circuits;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import sdc.spdz.circuits.operation.Gate;
 
@@ -12,112 +11,65 @@ import sdc.spdz.circuits.operation.Gate;
 public class CircuitGenerator {
 
    private final ArrayList<CircuitTriple> triples;
-   private int n_fios_entrada;
    private final int P;
-   private int[] fios_entrada;
-   private final ArrayList<Boolean> ocupacao;
+   private final ArrayList<Integer> notOccupied;
    private Gate gate;
-   private int aresta2;
-   private int aresta1;
+   private int edgeOne;
+   private int edgeTwo;
+   private int edgeCount;
 
    public CircuitGenerator(int P) {
       this.P = P;
-      this.ocupacao = new ArrayList<>();
+      this.notOccupied = new ArrayList<>();
       this.triples = new ArrayList<>();
    }
 
-   public Circuit generate(int n_fios_entrada) {
-      this.n_fios_entrada = n_fios_entrada;
-      this.fios_entrada = new int[n_fios_entrada];
+   public Circuit generate(int NINPUTS) {
+      edgeCount = NINPUTS;
+      for (int i = 0; i < NINPUTS; i++) {
+         notOccupied.add(i);
+      }
       stateOne();
 
       CircuitTriple[] cts = new CircuitTriple[triples.size()];
       for (int i = 0; i < triples.size(); i++) {
          cts[i] = triples.get(i);
       }
-      Circuit circuit = new Circuit(n_fios_entrada, cts);
+
+      Circuit circuit = new Circuit(NINPUTS, cts);
       return circuit;
    }
 
    private void stateOne() {
-      Random random = new Random();
-
-      for (int i = 0; i < n_fios_entrada; i++) {
-         fios_entrada[i] = random.nextInt(P);
-      }
+      //Random random = new Random();
+      //gate = random.nextBoolean() ? Gate.PLUS : Gate.MULT;
+      gate = Gate.PLUS;
 
       stateTwo();
    }
 
    private void stateTwo() {
-      for (int i = 0; i < n_fios_entrada; i++) {
-         ocupacao.add(false);
-      }
+      Random random = new Random();
 
+      int i = random.nextInt(notOccupied.size());
+      edgeOne = notOccupied.get(i);
+      notOccupied.remove(i);
+
+      i = random.nextInt(notOccupied.size());
+      edgeTwo = notOccupied.get(i);
+      notOccupied.remove(i);
+
+      notOccupied.add(edgeCount++);
+      
       stateThree();
    }
 
    private void stateThree() {
-      Random random = new Random();
-      //gate = random.nextBoolean() ? Gate.PLUS : Gate.MULT;
-      gate = Gate.PLUS;
-      //System.out.println(Arrays.toString(ocupacao.toArray()));
-
-      stateFour();
-   }
-
-   private void stateFour() {
-      ArrayList<Integer> livres = new ArrayList();
-      for (int i = 0; i < ocupacao.size(); i++) {
-         if (!ocupacao.get(i)) {
-            livres.add(i);
-         }
-      }
-
-      Random random = new Random();
-
-      int i = random.nextInt(livres.size());
-      aresta1 = livres.get(i);
-      livres.remove(i);
-
-      i = random.nextInt(livres.size());
-      aresta2 = livres.get(i);
-      livres.remove(i);
-
-      stateFive();
-   }
-
-   private void stateFive() {
-      ocupacao.set(aresta1, true);
-      ocupacao.set(aresta2, true);
-
-      stateSix();
-   }
-
-   private void stateSix() {
-      ocupacao.add(false);
-
-      stateSeven();
-   }
-
-   private void stateSeven() {
-      CircuitTriple ct = new CircuitTriple(P, gate, aresta1, aresta2);
+      CircuitTriple ct = new CircuitTriple(P, gate, edgeOne, edgeTwo);
       triples.add(ct);
 
-      if (!completo()) {
-         stateThree();
+      if (notOccupied.size() > 1) {
+         stateOne();
       }
    }
-
-   private boolean completo() {
-      int count = 0;
-      for (Boolean b : ocupacao) {
-         if (!b) {
-            count++;
-         }
-      }
-
-      return count == 1;
-   }
-
 }
